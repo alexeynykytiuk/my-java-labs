ab26ba6133d499244abb633647dea7d30a243a0f
Alexey Nykytiuk <nikityuk.alexey@gmail.com>
2023-11-25 01:03:39 +0200

    lab3: added Runner

---
+++ src/main/java/edu/ntudp/pzks/thirdLab/Runner.java
@@ -0,0 +1,9 @@
+package edu.ntudp.pzks.thirdLab;
+
+import edu.ntudp.pzks.thirdLab.controllers.UniversityCreator;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(UniversityCreator.createTypicalUniversity());
+    }
+}
